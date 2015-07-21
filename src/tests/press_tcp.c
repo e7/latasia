a7df5c4b193418a4447efbca516346cb63db61be
e7 <ryuuzaki.uchiha@gmail.com>
2015-07-21 19:31:02 +0800

    press_tcp实现命令行参数解析

--- src/tests/press_tcp.c
+++ src/tests/press_tcp.c
@@ -1,29 +1,75 @@
-#if 0
+#if 1
 #include <common.h>
+#include <pthread.h>
+
+
+#define USAGE "usage: %s -s server -p port -c num -t time\n"
+
+
+static void *thread_proc(void *args)
+{
+    return NULL;
+}
 
 
 int main(int argc, char *argv[], char *env[])
 {
     int opt;
+    int nthreads = 1; // 默认一个线程
+    int nsec = 5; // 默认运行5秒
+    int nport;
 
-    while ((opt = getopt(argc, argv, "nt:")) != -1) {
+    if (1 == argc) {
+        (void)fprintf(stderr, USAGE, argv[0]);
+        return EXIT_SUCCESS;
+    }
+
+    while ((opt = getopt(argc, argv, "s:p:c:t:")) != -1) {
         switch (opt) {
-        case 'n': {
+        case 's': {
+            break;
+        }
+
+        case 'p': {
+            nport = atoi(optarg);
+            if ((nport < 1) || (nport > 65535)) {
+                (void)fprintf(stderr, "invalid argument for -p\n");
+                return EXIT_FAILURE;
+            }
+
+            break;
+        }
+
+        case 'c': {
+            nthreads = atoi(optarg);
+            if ((nthreads < 1) || (nthreads > 99999)) {
+                (void)fprintf(stderr, "invalid argument for -c\n");
+                return EXIT_FAILURE;
+            }
+
+            (void)fprintf(stderr, "nthreads=%d", nthreads);
             break;
         }
 
         case 't': {
+            nsec = atoi(optarg);
+            if ((nsec < 1) || (nsec > 99999)) {
+                (void)fprintf(stderr, "invalid argument for -t\n");
+                return EXIT_FAILURE;
+            }
+
+            (void)fprintf(stderr, "nsec=%d\n", nsec);
             break;
         }
 
         default: {
-            (void)fprintf(stderr, "usage: .....\n");
+            (void)fprintf(stderr, USAGE, argv[0]);
             break;
         }
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 #else
