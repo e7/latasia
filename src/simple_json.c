447ebfc0a539226a604fb22d8d1d793fc585ae85
e7 <jackzxty@126.com>
2015-12-09 12:16:38 +0000

    开始实现simple_json解码(1)

--- src/simple_json.c
+++ src/simple_json.c
@@ -21,5 +21,10 @@ int lts_sjon_encode(lts_sjson_t *sjson, lts_str_t *output)
 
 int lts_sjon_decode(lts_str_t *src, lts_sjson_t *output)
 {
+    uint8_t invisible[] = {'\t', '\n', '\r', '\x20'};
+
+    // 过滤不可见字符
+    (void)lts_str_filter_multi(src, invisible, ARRAY_COUNT(invisible));
+
     return 0;
 }
