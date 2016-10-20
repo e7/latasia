ce4bb3c152934a3308879ac63783d6a675a75292
e7 <jackzxty@126.com>
2016-10-20 14:36:17 +0800

    整理字符串测试代码

--- src/tests/test_adv_string.c
+++ src/tests/test_adv_string.c
@@ -1,7 +1,7 @@
 #include "adv_string.h"
 
 
-int main(void)
+void common_test(void)
 {
     uint8_t s[] = " abc defg\tasfaf\n";
     uint8_t target[] = " a\t\n";
@@ -16,6 +16,30 @@ int main(void)
     fprintf(stderr, "[%s]\n", s);
     lts_str_filter_multi(&str, target, sizeof(target));
     fprintf(stderr, "[%s]\n", s);
+
+    return;
+}
+
+
+void find_test(void)
+{
+    uint8_t s[] = "s@#a";
+    uint8_t p1[] = "aaaaasfaf32";
+    uint8_t p2[] = "@#";
+
+    lts_str_t ls_s = lts_string(s);
+    lts_str_t ls_p1 = lts_string(p1);
+    lts_str_t ls_p2 = lts_string(p2);
+
+    fprintf(stderr, "find test: %d\n", lts_str_find(&ls_s, &ls_p1, 0));
+    fprintf(stderr, "find test: %d\n", lts_str_find(&ls_s, &ls_p2, 0));
+}
+
+
+int main(void)
+{
+    common_test();
+    find_test();
 
     return 0;
 }
