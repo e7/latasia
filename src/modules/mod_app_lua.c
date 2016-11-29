eb9b3bba1a48fbd054d00b516ff2517a7727f934
e7 <jackzxty@126.com>
2016-11-29 23:10:13 +0800

    增加内置lua5.1依赖

--- src/modules/mod_app_lua.c
+++ src/modules/mod_app_lua.c
@@ -4,9 +4,9 @@
 
 #include <strings.h>
 
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+#include "lua/lua.h"
+#include "lua/lualib.h"
+#include "lua/lauxlib.h"
 
 #include "latasia.h"
 #include "rbtree.h"
