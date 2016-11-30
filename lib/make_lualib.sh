190c9f9eb5177c8007903cac9fd635c0ed3847ef
e7 <jackzxty@126.com>
2016-11-30 09:16:00 +0800

    移除lua二进制静态库，改用动态编译的方式

---
+++ lib/make_lualib.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# extract
+tar xvf lua-5.1.5.tar.gz
+ln -svf lua-5.1.5 lua
+
+# make
+cd lua
+make linux
+cd ../..
+
+# copy .h and .a file
+header_dir="include/lua"
+if [[ ! -d "${header_dir}" ]] ; then
+    mkdir -p "${header_dir}"
+fi
+
+cp lib/lua/src/lua.h ${header_dir}
+cp lib/lua/src/lualib.h ${header_dir}
+cp lib/lua/src/luaconf.h ${header_dir}
+cp lib/lua/src/lauxlib.h ${header_dir}
+cp lib/lua/src/liblua.a lib
